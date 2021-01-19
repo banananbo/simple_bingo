9096c3c38d8cdf452f92c1fe99ddb289d54aa769
banananbo <banananbo@gmail.com>
2021-01-19 10:51:30 +0900

    timer test

--- src/BingoMain.vue
+++ src/BingoMain.vue
@@ -4,14 +4,14 @@
     <div class="container-fluid text-center">
      <BingoView :bingo="this.$store.state.bingo" :size="size" @cellClick='onCellClicked'></BingoView>
     </div>
-    <div v-if="!this.$store.state.bingo.is_playing">
-        <button @click="startGame">START!</button>
+    <div v-if="!this.$store.state.bingo.is_playing" class="text-center">
+        <button @click="startGame" class="btn btn-default">START!</button>
     </div>
     <div v-if="this.$store.state.bingo.is_playing">
-        <ul>
-            <li>score:{{this.$store.state.bingo.score}}</li>
-            <li>bingo:{{this.$store.state.bingo.bingonum}}</li>
-            <li>time:{{this.$store.state.bingo.time}}</li>
+        <ul class="list-group list-group-horizontal card-text mx-auto" style="max-width: 80%;">
+                <li class="list-group-item">score:{{this.$store.state.bingo.score}}</li>
+                <li class="list-group-item">bingo:{{this.$store.state.bingo.bingonum}}</li>
+                <li class="list-group-item">time:{{timer}}</li>
         </ul>
     </div>
     <ControlPop v-if="cellPop" :cell="selectedCell" @submit="submitCell" @cancel="cancellCell"></ControlPop>
@@ -30,7 +30,9 @@ import ControlPop from "./ControlPop.vue";
 export type DataType ={
     size: number,
     cellPop: Boolean,
-    selectedCell: Cell
+    selectedCell: Cell,
+    timer:string,
+    timerObj:any
 }
 
 export default Vue.extend({
@@ -38,12 +40,14 @@ export default Vue.extend({
         return {
             size: screen.width - 40,
             cellPop: false,
-            selectedCell: null
+            selectedCell: null,
+            timer: "",
+            timerObj: null
         };
     },
     computed:{
 
-  },
+    },
 
     props: {
         // cell_size: {
@@ -69,7 +73,9 @@ export default Vue.extend({
     methods: {
         startGame(){
             this.$store.state.bingo.startGame();
-        },
+            this.timerObj = setInterval(() => {(this).timer = Math.floor(this.$store.state.bingo.time/3600000)+":"+Math.floor(this.$store.state.bingo.time/60000)
+            +":"+Math.floor(this.$store.state.bingo.time/1000)}, 1000);
+         },
         onCellClicked: function(obj:any){
             console.log(1);
             console.log(this.$store.state.bingo.is_playing);
