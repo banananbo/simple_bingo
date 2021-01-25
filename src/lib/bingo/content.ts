8cc656391e3dc059ed364234d0e451456accd8a9
banananbo <banananbo@gmail.com>
2021-01-25 11:48:24 +0900

    add img and change content structue (without imgs)

--- src/lib/bingo/content.ts
+++ src/lib/bingo/content.ts
@@ -1,57 +1,65 @@
 export class Content{
-    constructor(public id:number,public img_src:string, public title:string,public caption:string){
+
+    constructor(public id:number,public title:string,public caption:string){
+    }
+    static IMG_HOST:string = 'https://img.bin5.xyz/public/img/'
+    public get img_src(){
+        return  `${Content.IMG_HOST}contents/${('00'+this.id).slice(-3)}.png`
     }
     static contents:Array<Content> = [
-            new Content(1,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/001.png",
+            new Content(1,      
             'ねこ','いつものばしょにいるかも'),
             new Content(2,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/002.png",
             'ポスト','ゆうびんきょくのまえにはある'),
             new Content(3,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/003.png",
             'とけい','こうえんにあるかな'),
             new Content(4,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/004.png",
             'あかいはな','きれい'),
             new Content(5,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/005.png",
             'からす','そらをとんでるかも'),
             new Content(6,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/006.png",
             'きいろいはな','きれい'),
             new Content(7,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/007.png",
             'つき','おそらのどこかにみえるかな'),
             new Content(8,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/008.png",
             'パイロン','どうろにおいてあるかも'),
             new Content(9,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/009.png",
             'いぬ','さんぽしていないかな'),
             new Content(10,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/010.png",
             'ちょうちょ','はなのちかくにいるよ'),
             new Content(11,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/011.png",
             'あり','じめんをよくみてね'),
             new Content(12,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/012.png",
             'あおいくるま','きをつけてみつけよう'),
             new Content(13,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/013.png",
             '７のもじ','どこにあるかな'),
             new Content(14,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/014.png",
             'あかいくるま','めだつ'),
             new Content(15,
-                "https://banananbo.github.io/simple_bingo/public/img/contents/015.png",
             'だんごむし','いしのしたにいるかも'),
+            new Content(16,
+            'しんごう','どうろをさがそう'),
+            new Content(17,
+            'じはんき','じどうはんばいき'),
+            new Content(18,
+            'はと','ふんにちゅうい'),
+            new Content(19,
+            'むしくいば','あながあいているよ'),
+            new Content(20,
+            'ぼうし','おしゃれなひとをさがそう'),
+            new Content(21,
+            'じてんしゃ','かっこいい'),
+            new Content(22,
+            'けいトラ','トラック'),
+            new Content(23,
+            'ふたり','ふたりであるいているひとをさがそう'),
+            new Content(24,
+            'きのみ','こうえんにあるかな'),
+
     ];
 
     static get blank():Content{
         return new Content(0,
-            'https://h238net.com/wp-content/plugins/ninja-forms/assets/img/no-image-available-icon-6.jpg',
             'blank','caption'
         );
     }
@@ -61,6 +69,6 @@ export class Content{
         return this.contents[id];
     }
     static createByObj(obj:any):Content{
-        return new Content(obj.id,obj.img_src,obj.title,obj.caption);
+        return new Content(obj.id,obj.title,obj.caption);
     }
 }
